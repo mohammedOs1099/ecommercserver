621d7e184285a2b9f71dcff68fe7c0baa5332bfb
mohammedOs1099 <mohammed.osama1099@gmail.com>
2025-02-19 20:43:05 +0200

    frist1

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
+server.use(jsonServer.defaults());
+server.use(auth);
 server.use(router);
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+server.listen(5000, () => {
+  console.log("JSON Server is running on port 5000");
 });
